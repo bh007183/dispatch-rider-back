c25350bc83ae2aa1b4ed2fe711b132b18f35415a
Ben Hopkins <bjhops17@gmail.com>
2021-03-22 15:18:26 -0700

    added create account route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -8,4 +8,19 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 
+router.post("/api/create/account", async (req, res) => {
+    console.log(req)
+    let encrypted = await bcrypt.hash(req.body.password, saltRounds)
+   let data = await db.User.create({
+       firstName: req.body.firstName,
+       lastName: req.body.lastName,
+       username: req.body.username,
+       firstName: req.body.firstName,
+       email: req.body.email,
+       password: encrypted,
+   }).catch(err => res.json(err))
+   res.json(data).end()
+})
+
+
 module.exports = router;
